01243ddc3d12d586ad896aa2e83653e997404fee
Kot__Mika <97839696+Artyom242@users.noreply.github.com>
2023-03-06 18:06:28 +0700

    Delete forms directory

--- forms/form.php
+++
@@ -1,8 +0,0 @@
-<?php
-$comm = $_POST['comm'];
-
-$newList = file_get_contents($_SERVER['DOCUMENT_ROOT']. '/comments.txt');
-$data = date("d.m.y H:i:s");
-file_put_contents( $_SERVER['DOCUMENT_ROOT'] .'/comments.txt', "Заявка от: $data \n$comm \n$newList");
-
-header('Location: /8_mar_2023');
